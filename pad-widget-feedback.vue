525fd09e6aef666fa81efdd46a968abb66bf438a
Zoltan Ruzman <zoltan.ruzman@msg.group>
2022-10-14 16:45:09 +0200

    30sec timeout for feedback

--- pad-widget-feedback.vue
+++ pad-widget-feedback.vue
@@ -2,31 +2,37 @@
   <section id="feedback">
     <h2 id="title" style="grid-area: title">Feedback</h2>
     <button style="grid-area: button1"
+            v-bind:disabled="isFeedbackDisabled"
             @click="sendFeedback('thumbsup')">
       <i class="icon fas fa-thumbs-up"></i>
       <span class="title">Show Consent</span>
     </button>
     <button style="grid-area: button2"
+            v-bind:disabled="isFeedbackDisabled"
             @click="sendFeedback('thumbsdn')">
       <i class="icon fas fa-thumbs-down"></i>
       <span class="title">Show Refusal</span>
     </button>
     <button style="grid-area: button3"
+            v-bind:disabled="isFeedbackDisabled"
             @click="sendFeedback('surprise')">
       <i class="icon fas fa-surprise"></i>
       <span class="title">Show Surprise</span>
     </button>
     <button style="grid-area: button4"
+            v-bind:disabled="isFeedbackDisabled"
             @click="sendFeedback('smile')">
       <i class="icon fas fa-grin-wink"></i>
       <span class="title">Show Smile</span>
     </button>
     <button style="grid-area: button5"
+            v-bind:disabled="isFeedbackDisabled"
             @click="sendFeedback('frown')">
       <i class="icon fas fa-angry"></i>
       <span class="title">Show Frown</span>
     </button>
     <button style="grid-area: button6"
+            v-bind:disabled="isFeedbackDisabled"
             @click="sendFeedback('sadness')">
       <i class="icon fas fa-sad-tear"></i>
       <span class="title">Show Sadness</span>
@@ -96,9 +102,19 @@ button {
 <script>
 module.exports = {
   name: "pad-widget-feedback",
+  data: () => ({
+    isFeedbackDisabled: false
+  }),
   methods: {
-    sendFeedback (value) {
+    sendFeedback(value) {
+      if (this.isFeedbackDisabled)
+        return
+
+      this.isFeedbackDisabled = true
       this.huds.sendFeedback(value)
+      setTimeout(() => {
+        this.isFeedbackDisabled = false
+      }, 30 * 1000)
     }
   }
 }
