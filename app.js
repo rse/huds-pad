500b50d680eecf600f8dd691fb6baa50be0465c3
Ralf S. Engelschall <rse@engelschall.com>
2022-10-16 11:00:39 +0200

    more main procedure to JS file

--- app.js
+++ app.js
@@ -128,3 +128,61 @@ class HUDS {
     }
 }
 
+class App {
+    static main () {
+        /*  initialize user interface  */
+        Vue.load = (name) => {
+            return window["vue3-sfc-loader"].loadModule(name, {
+                moduleCache: { vue: Vue, less: less },
+                getFile (url) {
+                    return fetch(url).then((res) => res.ok ? res.text() :
+                        Promise.reject(Object.assign(new Error(`${url}: ${res.statusText}`), { res })))
+                },
+                addStyle (textContent) {
+                    const style = Object.assign(document.createElement("style"), { textContent })
+                    const ref = document.head.getElementsByTagName("style")[0] || null
+                    document.head.insertBefore(style, ref)
+                },
+                log (type, ...args) {
+                    console.log("vue3-sfc-loader", type, ...args)
+                },
+                additionalModuleHandlers: {
+                    ".css": (source, path, options) => {
+                        return less.render(source, { globalVars: vars }).then((result) => result.css)
+                    }
+                }
+            })
+        }
+        Vue.loadComponent = (name) =>
+            Vue.defineAsyncComponent(() => Vue.load(name))
+        let app = Vue.createApp({ components: { "app": Vue.loadComponent("app.vue") } })
+        app.mount("#app")
+
+        /*  provide global properties  */
+        const info   = Vue.ref("")
+        const status = Vue.reactive({
+            connected:           false,
+            isMessagingDisabled: false,
+            isVotingDisabled:    true,
+            votingType:          "propose",
+            clearVoting:         false
+        })
+        const huds = new HUDS("app.yaml")
+        app.config.globalProperties.huds = huds
+        app.config.globalProperties.$status = {
+            value: status,
+            setConnectionEstablished ()  { status.connected = true  },
+            setConnectionClosed ()       { status.connected = false },
+            disabledMessaging (disabled) { status.isMessagingDisabled = disabled },
+            disabledVoting (disabled)    { status.isVotingDisabled    = disabled },
+            setVotingType (type)         { status.votingType = type },
+            clearVoting ()               { status.clearVoting = !status.clearVoting }
+        }
+        app.config.globalProperties.$info = {
+            value: info,
+            setMessage (text) { info.value = text },
+            clearMessage ()   { info.value = "" }
+        }
+    }
+}
+
