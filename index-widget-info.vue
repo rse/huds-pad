d5af7f852a4d8faa49e956abf060db123911128f
Ralf S. Engelschall <rse@engelschall.com>
2022-10-16 10:22:28 +0200

    restyle status

--- index-widget-info.vue
+++ index-widget-info.vue
@@ -1,14 +1,15 @@
 
 <template>
-    <p class="info">{{ $info.value.value }}</p>
+    <div class="info">{{ $info.value.value }}</div>
 </template>
 
 <style lang="less" scoped>
     .info {
-        margin-top: 20px;
+        margin-top: 8px;
         width: 100%;
+        font-weight: 200;
         text-align: center;
-        color: var(--color-std-fg-0);
+        color: var(--color-std-fg-1);
     }
 </style>
 
