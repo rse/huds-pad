ee5d118dc1c6eb6eb46bb1a6f92314e8b9230957
Ralf S. Engelschall <rse@engelschall.com>
2022-10-16 12:57:33 +0200

    code cleanup

--- app-head.vue
+++ app-head.vue
@@ -1,8 +1,8 @@
 
 <template>
-    <div class="pad-widget-title">
+    <div class="app-pad-head">
         <div class="back">
-            <button v-show="showDisconnect" @click="disconnect()">
+            <button v-show="$status.value.connected" @click="disconnect()">
                 <i class="icon fas fa-arrow-alt-circle-left"></i><br>
             </button>
         </div>
@@ -11,58 +11,53 @@
 </template>
 
 <style lang="less" scoped>
-.pad-widget-title {
+.app-pad-head {
     margin-top: 8px;
     display: flex;
     flex-direction: row;
-}
-
-button {
-    line-height: 12pt;
-    background: none;
-    cursor: pointer;
-    border: 1px solid transparent;
-
-    .icon {
-        display: inline-block;
-        color: var(--color-std-fg-1);
-        font-size: 18pt;
-        width: 25px;
-        text-align: center;
-    }
-
-    &:hover, &:focus {
-        border: 0;
-        outline: none;
-        color: var(--color-sig-fg-5);
-        background-color: var(--color-sig-bg-3);
-        border-top: 1px solid var(--color-sig-bg-5);
-        border-left: 1px solid var(--color-sig-bg-5);
-        border-right: 1px solid var(--color-sig-bg-1);
-        border-bottom: 1px solid var(--color-sig-bg-1);
+    button {
+        line-height: 12pt;
+        background: none;
+        cursor: pointer;
+        border: 1px solid transparent;
         .icon {
-            color: var(--color-sig-fg-1);
+            display: inline-block;
+            color: var(--color-std-fg-1);
+            font-size: 18pt;
+            width: 25px;
+            text-align: center;
+        }
+        &:hover, &:focus {
+            border: 0;
+            outline: none;
+            color: var(--color-sig-fg-5);
+            background-color: var(--color-sig-bg-3);
+            border-top: 1px solid var(--color-sig-bg-5);
+            border-left: 1px solid var(--color-sig-bg-5);
+            border-right: 1px solid var(--color-sig-bg-1);
+            border-bottom: 1px solid var(--color-sig-bg-1);
+            .icon {
+                color: var(--color-sig-fg-1);
+            }
         }
     }
-}
-
-.back {
-    width: 40px;
-}
-
-h1 {
-    line-height: 0;
-    margin-top: 2px;
-    margin-bottom: 20px;
-    text-align: center;
-    flex-grow: 1;
-    position: relative;
-    left: -20px;
-    .icon {
-        display: inline-block;
-        color: var(--color-std-fg-1);
-        font-size: 16pt;
-        margin-right: 8px;
+    .back {
+        width: 40px;
+    }
+    h1 {
+        line-height: 0;
+        margin-top: 2px;
+        margin-bottom: 20px;
+        text-align: center;
+        flex-grow: 1;
+        position: relative;
+        left: -20px;
+        .icon {
+            display: inline-block;
+            color: var(--color-std-fg-1);
+            font-size: 16pt;
+            margin-right: 8px;
+        }
     }
 }
 </style>
@@ -70,11 +65,10 @@ h1 {
 <script>
 module.exports = {
     name: "app-head",
-    props: [ "showDisconnect" ],
     methods: {
         disconnect () {
             this.huds.endAttendance()
-            this.huds.client.on("close", () => {
+            this.huds.client.once("close", () => {
                 window.location.hash = ""
                 this.$info.clearMessage()
                 this.$status.setConnectionClosed()
