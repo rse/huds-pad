016117b44e4d492650824445c5dcf49520e14647
Zoltan Ruzman <zoltan.ruzman@msg.group>
2022-10-17 17:52:05 +0200

    remove name and image from attendance

--- app-huds.js
+++ app-huds.js
@@ -94,8 +94,6 @@ window.HUDS = class HUDS {
         return this.sendMessageToBroker("attendance", {
             event: "begin",
             data: {
-                name:    this.client.id,
-                image:   "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
                 privacy: "public"
             }
         })
