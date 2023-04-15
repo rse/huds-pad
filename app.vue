ce311bda785be9330c9a635957318e8560323674
Ralf S. Engelschall <rse@engelschall.com>
2023-04-15 16:57:06 +0200

    ensure background is large enough

--- app.vue
+++ app.vue
@@ -44,8 +44,8 @@
 .app
     margin: 0
     padding: 0
-    width: 100vw
-    height: 100vh
+    width: 100%
+    height: 100%
     font-family: "TypoPRO Source Sans Pro", sans-serif
     background-color: var(--color-std-bg-1)
     color: var(--color-std-fg-3)
